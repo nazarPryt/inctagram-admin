91d8d0d3e42d216885a49ac162757900edf1a0e3
nazarPtyt <ockap90@libero.it>
2023-12-06 18:15:42 +0100

    fix: SSR Styled-component (to obtain already rendered page)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,39 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html lang={'en'}>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+
+    try {
+      ctx.renderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+      })
+      const initialProps = await Document.getInitialProps(ctx)
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
---
+++ src/shared/Button.tsx
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components'
+
+const Wrapper = styled.button`
+  color: white;
+  cursor: pointer;
+  width: 400px;
+
+  line-height: 1.4;
+  border-radius: 10px;
+  background-color: royalblue;
+`
+
+export const Button = () => {
+  return <Wrapper>dfdf</Wrapper>
+}
